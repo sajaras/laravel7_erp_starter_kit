aa1ca4c060c33bc273783e2b74e7c9e9236a3c35
sajaras <sajras1992@gmail.com>
2024-10-29 08:25:07 +0530

    updating left side bar icons

--- resources/views/layouts/leftsidebars/master.blade.php
+++ resources/views/layouts/leftsidebars/master.blade.php
@@ -90,9 +90,7 @@
     <li class="menu">
         <a href="{{route('users')}}" aria-expanded="false" class="dropdown-toggle">
             <div class="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square">
-                    <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                </svg>
+            @component('svg_icons.users') @slot('width')24 @endslot @slot('height') 24 @endslot @endcomponent
                 <span>Users</span>
             </div>
         </a>
@@ -101,10 +99,7 @@
     <li class="menu">
         <a href="{{route('roles')}}" aria-expanded="false" class="dropdown-toggle">
             <div class="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
-                    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-                    <polyline points="22,6 12,13 2,6"></polyline>
-                </svg>
+            @component('svg_icons.roles') @slot('width')24 @endslot @slot('height') 24 @endslot @endcomponent
                 <span>Roles</span>
             </div>
         </a>
@@ -117,6 +112,24 @@
                     <polyline points="22,6 12,13 2,6"></polyline>
                 </svg>
                 <span>Permissions</span>
+            </div>
+        </a>
+    </li>
+
+    <li class="menu menu-heading">
+        <div class="heading"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus">
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+            </svg><span>Logs </span></div>
+    </li>
+
+    <li class="menu">
+        <a href="{{route('permissions')}}" aria-expanded="false" class="dropdown-toggle">
+            <div class="">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+                    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                    <polyline points="22,6 12,13 2,6"></polyline>
+                </svg>
+                <span>Audit Log</span>
             </div>
         </a>
     </li>
